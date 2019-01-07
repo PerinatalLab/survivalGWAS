6517c38aad582d661955e93cc6acd637584a4ccb
Pol Sole Navais <pol.sole.navais@plab-home.novalocal>
2019-01-07 10:41:17 +0100

    added icd codes

--- scripts/hunt/pheno_HUNT.R
+++ scripts/hunt/pheno_HUNT.R
@@ -17,6 +17,7 @@ final_vars_PROM_MOR= c('MOR_PID', 'SVLEN_DG', 'PROM', 'PC1', 'PC2', 'PC3', 'PC4'
 final_vars_PROM_BARN= c('BARN_PID', 'SVLEN_DG', 'PROM', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'FAAR', 'PARITY0')
 final_vars_spont_MOR= c('MOR_PID', 'SVLEN_DG', 'spont', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'FAAR', 'PARITY0')
 final_vars_spont_BARN= c('BARN_PID', 'SVLEN_DG', 'spont', 'PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'FAAR', 'PARITY0')
+icd_path= '/mnt/archive/hunt/phenotypes/hnt/Gestational age_AP_Avidentifisert.txt'
 
 SelectRelated= function(kin_path, sample_list, df, var){
   kin= read.table(kin_path, h=T, comment.char = "", sep= '\t')
@@ -56,10 +57,11 @@ pc_fets= read.table(paste0(pc_path, 'fetal_pca.sscore'), h=F, sep= '\t')
 names(pc_moms)= c('MOR_PID', 'IID','X','X1','PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10')
 names(pc_fets)= c('BARN_PID', 'IID','X','X1','PC1','PC2','PC3','PC4','PC5','PC6','PC7','PC8','PC9','PC10')
 
-# PROM
+'''
+# PROM (initial phenotype. Not used)
 
 final= mutate(mfr, PROM = as.numeric(!is.na(VANNAVGANG)), PARITY0= as.numeric(PARITET_MFR==0))
-final= filter(final, is.na(FLERFODSEL), DODKAT<6 | DODKAT>10, !is.na(SVLEN_DG), SVLEN_DG<308 & SVLEN_DG>154 & !is.na(PROM), (VANNAVGANG<3 | is.na(VANNAVGANG)))
+final= filter(final, is.na(FLERFODSEL), DODKAT<6 | DODKAT>10, !is.na(SVLEN_DG), SVLEN_DG<308 & SVLEN_DG>154 & !is.na(PROM))
 
 final = filter(final, is.na(ART),is.na(ABRUPTIOP),
                            is.na(PLACENTA_PREVIA),
@@ -95,6 +97,7 @@ write.table(moms, paste0(outpath, file_pref_PROM, '_moms'), row.names=F, col.nam
 write.table(moms_sens, paste0(outpath, file_pref_PROM, '_moms_sens'), row.names=F, col.names=T, sep= '\t', quote=F)
 write.table(fets, paste0(outpath, file_pref_PROM, '_fets'), row.names=F, col.names=T, sep= '\t', quote=F)
 write.table(fets_sens, paste0(outpath, file_pref_PROM, '_fets_sens'), row.names=F, col.names=T, sep= '\t', quote=F)
+'''
 
 ### Spontaneous delivery
 
@@ -170,3 +173,97 @@ write.table(moms, paste0(outpath, file_pref_spont, '_moms'), row.names=F, col.na
 write.table(moms_sens, paste0(outpath, file_pref_spont, '_moms_sens'), row.names=F, col.names=T, sep= '\t', quote=F)
 write.table(fets, paste0(outpath, file_pref_spont, '_fets'), row.names=F, col.names=T, sep= '\t', quote=F)
 write.table(fets_sens, paste0(outpath, file_pref_spont, '_fets_sens'), row.names=F, col.names=T, sep= '\t', quote=F)
+
+#### New pheno for PROM. Accorded with Bo and Nils on the 19/01/02
+# PROM diagnosis from MFR or ICD codes 9 or 10.
+# Following ICD codes:
+# ICD9:  658.1 and 658.2
+# ICD10: O42
+
+SelectRelated= function(kin_path, sample_list, df, var){
+  kin= read.table(kin_path, h=T, comment.char = "", sep= '\t')
+ kin= kin %>% filter(KINSHIP>0.044)
+ kin= kin %>% filter(X.FID1 %in% sample_list & FID2 %in% sample_list)
+ kin= kin %>% mutate(ID1= paste(X.FID1,ID1, sep= ":"),
+                      ID2= paste(FID2, ID2, sep= ":")) %>% select(ID1, ID2, KINSHIP)
+  kin_temp= kin
+  colnames(kin_temp)= c("ID2","ID1","KINSHIP")
+  kin_temp= rbind(kin_temp, kin)  
+  kin_temp= kin_temp %>% add_count(ID1)
+  kin_temp= kin_temp %>% add_count(ID2)
+df[['ID1']]= paste(df[[var]], df[[var]], sep=':')
+kin_temp= inner_join(kin_temp, df[,c('ID1','PROM')], by= 'ID1')
+  kin_temp= arrange(kin_temp, desc(PROM), n, nn)
+  to_keep= list()
+  
+  for (i in 1:nrow(kin_temp)) {
+    if (kin_temp[i,"ID1"] %in% unlist(kin_temp[0:i,"ID2"])) {
+      kin_temp[i,"ID2"]= "X"
+    }
+    else
+      to_keep[[i]] <- kin_temp[["ID1"]][i]
+  }
+  to_remove= kin_temp %>% filter(!(ID1 %in% unlist(to_keep))) %>% select(ID1) 
+  to_remove= to_remove[!duplicated(to_remove$ID1),]
+  to_remove= to_remove %>% separate(ID1, c('FID','ID'), sep=":")
+
+  return(unlist(to_remove[,1]))
+}
+
+icd= read.table(icd_path, h=T, sep= '\t', quote='')
+icd$FAAR= as.numeric(substring(icd$Diagnosedato, 1, 4))
+
+icd= mutate(icd, 
+		PROM_icd9= grepl('658.1|658.2', icd$ICD_9, fixed=F),
+		PROM_icd10= grepl('O42', icd$ICD_10, fixed=F))
+icd= icd %>% mutate(PROM_icd= ifelse(PROM_icd9== 1 | PROM_icd10== 1, 1, 0)) %>% select(PID.106764, FAAR, PROM_icd)
+
+final= mutate(mfr, PROM = as.numeric(!is.na(VANNAVGANG)), PARITY0= as.numeric(PARITET_MFR==0))
+
+final= left_join(final, icd, by= c('MOR_PID'='PID.106764', 'FAAR'))
+
+final= mutate(final, PROM= ifelse(PROM==1 | PROM_icd== 1, 1, 0))
+
+final= filter(final, is.na(FLERFODSEL), DODKAT<6 | DODKAT>10, !is.na(SVLEN_DG), SVLEN_DG<308 & SVLEN_DG>154 & !is.na(PROM), (VANNAVGANG<3 | is.na(VANNAVGANG)))
+
+final = filter(final, is.na(ART),is.na(ABRUPTIOP),
+                           is.na(PLACENTA_PREVIA),
+                        is.na(FOSTERV_POLYHYDRAMNION),
+                        is.na(MISD))
+
+final= final[order(final$PROM, decreasing= T),]
+
+# PROM sensitivity analysis will be PROM with spontaneous delivery
+
+final_sens= mutate(final, 
+		spont= as.numeric(FSTART==1 & (is.na(KSNITT) | KSNITT>1) &
+                (is.na(KSNITT_PLANLAGT) | KSNITT_PLANLAGT==1) &
+                is.na(INDUKSJON_PROSTAGLANDIN) & is.na(INDUKSJON_ANNET) &
+                is.na(INDUKSJON_OXYTOCIN) & is.na(INDUKSJON_AMNIOTOMI)))
+
+final_sens= mutate(final_sens, PROM= ifelse(spont== 1 & PROM==1, 1, 0))
+
+final_moms= inner_join(final, pc_moms, by= 'MOR_PID') %>% select(final_vars_PROM_MOR)
+final_fets= inner_join(final, pc_fets, by= 'BARN_PID') %>% select(final_vars_PROM_BARN)
+
+final_moms= final_moms[!duplicated(final_moms$MOR_PID),]
+final_fets= final_fets[!duplicated(final_fets$BARN_PID),]
+
+final_sens_moms= inner_join(final_sens, pc_moms, by= 'MOR_PID') %>% select(final_vars_PROM_MOR)
+final_sens_fets= inner_join(final_sens, pc_fets, by= 'BARN_PID') %>% select(final_vars_PROM_BARN)
+
+final_sens_moms= final_sens_moms[!duplicated(final_sens_moms$MOR_PID),]
+final_sens_fets= final_sens_fets[!duplicated(final_sens_fets$BARN_PID),]
+
+moms= final_moms %>% filter(!(MOR_PID %in% SelectRelated(paste0(kin_path, moms_kin), final_moms$MOR_PID,  final_moms, 'MOR_PID')))
+moms_sens= final_sens_moms %>% filter(!(MOR_PID %in% SelectRelated(paste0(kin_path, moms_kin), final_sens_moms$MOR_PID,  final_sens_moms, 'MOR_PID')))
+
+fets= final_fets %>% filter(!(BARN_PID %in% SelectRelated(paste0(kin_path, fets_kin), final_fets$BARN_PID, final_fets,'BARN_PID' )))
+fets_sens= final_sens_fets %>% filter(!(BARN_PID %in% SelectRelated(paste0(kin_path, fets_kin), final_sens_fets$BARN_PID, final_sens_fets, 'BARN_PID')))
+
+write.table(moms, paste0(outpath, file_pref_PROM, '_moms'), row.names=F, col.names=T, sep= '\t', quote=F)
+write.table(moms_sens, paste0(outpath, file_pref_PROM, '_moms_sens'), row.names=F, col.names=T, sep= '\t', quote=F)
+write.table(fets, paste0(outpath, file_pref_PROM, '_fets'), row.names=F, col.names=T, sep= '\t', quote=F)
+write.table(fets_sens, paste0(outpath, file_pref_PROM, '_fets_sens'), row.names=F, col.names=T, sep= '\t', quote=F)
+
+
